2c569f64ef303ad0df5fda2179e77789bf9fa778
danwald <danwald79@gmail.com>
2025-03-19 16:18:52 +0400

    adds social test

---
+++ tests/test_social.py
@@ -0,0 +1,18 @@
+import pytest
+
+from interfaces.social import Content
+
+class SocialTestContent(Content):
+    def __init__(self, data: str) -> None:
+        self.data = data
+
+
+class SocialTester:
+    def post(self, cnt: Content) -> bool:
+        return True
+
+
+def test_social():
+    assert SocialTester().post(
+        SocialTestContent('foobar')
+    )
