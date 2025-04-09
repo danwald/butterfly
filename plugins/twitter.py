56009bc63ff2db15cdee84f19a35363a9600fc47
danny crasto <danwald79@gmail.com>
2025-04-09 21:12:02 +0400

    WIP twitter

---
+++ plugins/twitter.py
@@ -0,0 +1,19 @@
+from typing import Any
+
+from plugins import Plugin
+
+
+class Twitter(Plugin):
+    def __init__(self) -> None:
+        self.name = "twitter"
+
+    def get_name(self) -> str:
+        return self.name
+
+    def execute(self, *args: tuple[Any], **kwargs: dict[str, Any]) -> bool:
+        print(f"{self.name} plugin's execute() called with {args},{kwargs}")
+        return True
+
+    def validate(self) -> bool:
+        print(f"{self.name} plugin's validate()")
+        return True
