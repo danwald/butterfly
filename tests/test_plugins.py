5abb8cf119a416ebdb16ae5fc6bed4447dc3d013
danny crasto <danwald79@gmail.com>
2025-05-13 20:37:59 +0400

    discover plugins return self to allow chaining

--- tests/test_plugins.py
+++ tests/test_plugins.py
@@ -10,11 +10,10 @@ from plugins import PluginManager
 def manager():
     current_dir = Path(__file__).resolve().parent
     sys.path.insert(0, str(current_dir))
-    return PluginManager(plugin_dir=current_dir / "plugins")
+    return PluginManager(plugin_dir=current_dir / "plugins").discover_plugins()
 
 
 def test_plugin_manager(manager):
-    manager.discover_plugins()
     assert manager._run_method("hello", "execute", "arg1", key="value")
     assert manager._run_method("hello", "validate", "arg1", key="value")
     assert manager._run_method("hello", "doesnt_exist") is False
